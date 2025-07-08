85c7df9d9278b81f2605c73a5d4c880924db6c7d
dazi2011 <uhh476301@gmail.com>
2025-07-08 10:00:11 +0800

    Initial Commit

---
+++ BitTwister/BitTwisterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitTwisterApp.swift
+//  BitTwister
+//
+//  Created by 吴达子 on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct BitTwisterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
